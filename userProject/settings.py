8452d63f2886ef94dcaf034c113317d391cffd21
tsorig <olkooop@gmail.com>
2021-03-30 12:20:56 +0300

    Add Docker

--- userProject/settings.py
+++ userProject/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'a44bje35j7-n+)c8jmgv1!8#5c4g$a5wtnmh!2ht3s7psu=lu1'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -23,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -78,6 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'PORT': '3306',
     }
 }
 
